32fa1ebd09bf23dedb9685f14e45e869a583d216
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-03-20 21:58:24 +0100

    Do not rerender image when card becomes invisible

--- igniteinator/src/components/CardImage.js
+++ igniteinator/src/components/CardImage.js
@@ -1,18 +1,31 @@
+import React from "react";
 import VisibilitySensor from "react-visibility-sensor";
 import { getFullImageUrl } from "../model";
 import "./CardImage.css";
 
+class CardImage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { load: false };
+    }
 
-const CardImage = ({ src }) => (
-    <VisibilitySensor partialVisibility>
-        {({ isVisible }) =>
-            isVisible ? (
-                <div><img src={getFullImageUrl(src)} alt="Card" /></div>
-            ) : (
-                <div className="unloadedCardImage"></div>
-            )
-        }
-    </VisibilitySensor>
-);
+    // Local state should be ok for this purpose.
+    onVisibilityChange = isVisible => {
+        if (isVisible && !this.state.load)
+            this.setState({ load: true })
+    }
+
+    render() {
+        return (
+            <VisibilitySensor partialVisibility onChange={this.onVisibilityChange}>
+                {this.state.load ? (
+                    <div><img src={getFullImageUrl(this.props.src)} alt="Card" /></div>
+                ) : (
+                    <div className="unloadedCardImage"></div>
+                )}
+            </VisibilitySensor>
+        );
+    }
+}
 
 export default CardImage;
