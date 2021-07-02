454c50c5217154fd0340047ef9aecfd911d2737d
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-07-02 20:12:25 +0200

    Fix Hideaway name

--- gen-data/1-fetch-data.sh
+++ gen-data/1-fetch-data.sh
@@ -11,3 +11,11 @@ curl "$BASE_URL/api/cards?lang_id=3" > "$CARDS_FRENCH_FILE"
 curl "$BASE_URL/api/cards?lang_id=4" > "$CARDS_GERMAN_FILE"
 curl "$BASE_URL/api/categories/1" > "$CAT1_FILE"
 curl "$BASE_URL/api/categories/2" > "$CAT2_FILE"
+
+# A little hack here: There's just one error in the data we need to correct.
+if [[ "$(grep -o HIdeaway "$CARDS_FILE" | wc -l)" -ne 1 ]]; then
+  echo "Did not find 1 instance of \"HIdeaway\" in $CARDS_FILE."
+  exit 1
+fi
+echo "Fixing Hideaway name ..."
+sed -i 's/HIdeaway/Hideaway/' "$DOWNLOAD_DIR"/*.json
