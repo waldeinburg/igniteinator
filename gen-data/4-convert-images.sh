b7524744472e5607158e72163dc52dc3ba17489c
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-07-02 22:05:23 +0200

    Gave up trimming for now

--- gen-data/4-convert-images.sh
+++ gen-data/4-convert-images.sh
@@ -14,6 +14,12 @@ find "$IMG_DOWNLOAD_DIR" -type f | while read -r f; do
   # Converting to JPG (ImageMagick) saves about 19MB but looses the transparent border.
   # Converting to GIF (ImageMagick) saves about 31MB but the color depth is insufficient.
   out="$dir/$id.png"
+  # I wished i could
+  #   convert "$f" -trim "$aux_file"
+  # but the images do not end up having the same size. Adding -fuzz does not work. Poison Arrow
+  # becomes 405x568 without fuzz while Monopoly becomes 406x567 at -fuzz 78% (x trimmed too little,
+  # y trimmed too much). Absolute values does not help (-fuzz 52000 is 406x567). For now, just
+  # accept the ~15px border.
   echo "$f -> $out"
   optipng -out "$out" "$f"
 done
