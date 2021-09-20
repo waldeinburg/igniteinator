4cdfa0e639fb2d8b62bd590dc08d335b1de5038d
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-09-20 19:20:37 +0200

    Styled footer to separate it

--- src/igniteinator/ui/footer.cljs
+++ src/igniteinator/ui/footer.cljs
@@ -1,14 +1,16 @@
 (ns igniteinator.ui.footer
   (:require [igniteinator.constants :refer [version page-url github-url]]
             [igniteinator.ui.link :refer [external-link]]
-            [reagent-material-ui.core.link :refer [link]]))
+            [reagent-material-ui.core.link :refer [link]]
+            [reagent-material-ui.core.box :refer [box]]))
 
 (defn footer []
-  [:footer
-   ;; No translation of this text.
-   [:p
-    "All data and images are Copyright © 2021, "
-    [external-link "https://gingersnapgaming.com" "Ginger Snap Gaming"]
-    ". Used in the " [link {:href page-url} "Igniteinator"] " by permission."]
-   [:p "Source code available on " [external-link github-url "GitHub"] "."]
-   [:p "Version " version "."]])
+  [box {:mt 6, :color :text.secondary}
+   [:footer
+    ;; No translation of this text.
+    [:p
+     "All data and images are Copyright © 2021, "
+     [external-link "https://gingersnapgaming.com" "Ginger Snap Gaming"]
+     ". Used in the " [link {:href page-url} "Igniteinator"] " by permission."]
+    [:p "Source code available on " [external-link github-url "GitHub"] "."]
+    [:p "Version " version "."]]])
