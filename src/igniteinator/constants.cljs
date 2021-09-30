5bb137ad542c8f7e0caf98a7e5fa615ece4a8564
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-09-30 15:15:15 +0200

    Bumped version

--- src/igniteinator/constants.cljs
+++ src/igniteinator/constants.cljs
@@ -1,6 +1,6 @@
 (ns igniteinator.constants)
 
-(def version "0.2.2")
+(def version "0.3.0")
 (def page-url "https://igniteinator.waldeinburg.dk")
 (def github-url "https://github.com/waldeinburg/igniteinator")
 (def img-base-path "/img")
