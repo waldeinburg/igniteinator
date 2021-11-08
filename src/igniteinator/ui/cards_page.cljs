23248c1d2b322d4af88deb02ee61d7610faea853
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-11-08 22:09:19 +0100

    Spacing between toolbar and cards list

--- src/igniteinator/ui/cards_page.cljs
+++ src/igniteinator/ui/cards_page.cljs
@@ -34,10 +34,11 @@
 
 (defn cards-page []
   [page (txt :cards-page-title)
-   [toolbar {:disable-gutters true}
-    [box {:mr 2}
-     [page-base-filtering]]
-    [search-bar
-     (<sub-ref :cards-page/search-str)
-     #(>evt :cards-page/set-search-str %)]]
+   [box {:mb 2}
+    [toolbar {:disable-gutters true}
+     [box {:mr 2}
+      [page-base-filtering]]
+     [search-bar
+      (<sub-ref :cards-page/search-str)
+      #(>evt :cards-page/set-search-str %)]]]
    [page-card-list]])
