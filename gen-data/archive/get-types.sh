312c91b110289ace21216c1ecfccd839ace02783
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2021-11-18 13:31:50 +0100

    fixup! Added data and script for adding types

--- gen-data/archive/get-types.sh
+++ gen-data/archive/get-types.sh
@@ -4,9 +4,8 @@
 cd "${0%/*}" || exit 1
 source ../common.inc.sh
 
-type_img=$(mktemp --suffix=".png")
-types_file=$(mktemp)
-trap 'rm -f "$type_img" "$types_file"' 0
+tmp_img=$(mktemp --suffix=".png")
+trap 'rm -f "$tmp_img"' 0
 
 jq -r '.cards[] | "\(.id)@\(.name)"' "../$BASE_DATA_FILE" | while read -r data; do
   id=$(cut -d'@' -f1 <<<"$data")
@@ -14,9 +13,9 @@ jq -r '.cards[] | "\(.id)@\(.name)"' "../$BASE_DATA_FILE" | while read -r data; 
   img="../$IMG_INPUT_DIR/en/$name.jpg"
   # Hatchet is widest.
   # Arrow with the short word "Projectile" gives the height but some images fail without a little extra margin.
-  convert "$img" -crop 2091x224+974+4998 "$type_img"
+  convert "$img" -crop 2091x224+974+4998 "$tmp_img"
   # psm 7: single text line
-  types=$(tesseract --psm 7 "$type_img" - 2>/dev/null | \
+  types=$(tesseract --psm 7 "$tmp_img" - 2>/dev/null | \
     # Remove junk characters, trim, replace the delimiter with comma, and quote.
     # The whitespace around the dash might not have been detected.
     # Tesseract gets some words wrong. Using --user-words with jq -r '.types[] | .name' does nothing.
